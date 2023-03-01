20ea4e4481607304b9bf1bc0bc784d27b4063678
Alxkahovsky <alxkahovsky@gmail.com>
2023-03-01 16:31:55 +0300

    done

--- menu/templatetags/menu_templatetag.py
+++ menu/templatetags/menu_templatetag.py
@@ -2,55 +2,60 @@ from django import template
 from menu.models import MenuItem, Menu
 from django.utils.safestring import mark_safe
 
+
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_slug):
+    menu_items = MenuItem.objects.filter(menu__slug=menu_slug)
+    menu_parents = menu_items.filter(parent=None)
     if context.request.GET:
+        print(context.request.GET)
         selected = context.request.GET['selected']
+        selected_item = menu_items.get(slug=selected)
+        branch_path = list(reversed(get_grandparents(selected_item, context)))
     else:
         selected = None
     out = ''
-    # menu = Menu.objects.filter(slug=menu_slug).order_by('-id')
+    out += f'<h2>{menu_slug}:</h2>'
     out += '<ul class="menu">'
-    for menu_main_items in MenuItem.objects.select_related('menu').filter(menu__slug=menu_slug, is_active=True,):
-        out += '<li class="menu-item" >'
-        out += f'<a href="{menu_main_items.get_absolute_url(context.request)}">{menu_main_items.title}</a>'
-        if selected == menu_main_items.slug:
-            out += draw_menu_parents(context, menu_main_items)
-            out += draw_menu_children(context, menu_main_items)
-        # out += draw_menu_children(menu_main_items, selected)
-        out += '</li>'
-    out += '</ul>'
+    for menu_parent_item in menu_parents:
+        if selected:
+            if menu_parent_item == branch_path[0]:
+                out += draw_branch(branch_path, context)
+            else:
+                out += '<li class="menu-item">'
+                out += f'<a href="{menu_parent_item.get_absolute_url(context.request)}">{menu_parent_item.title}</a>'
+        else:
+            out += '<li class="menu-item">'
+            out += f'<a href="{menu_parent_item.get_absolute_url(context.request)}">{menu_parent_item.title}</a>'
     return mark_safe(out)
 
 
 def draw_menu_children(context, menu_item):
     out = ''
-    #drawing the sub items
     out += '<ul class="submenu">'
-    for submenu_item in menu_item.children.all().order_by('-id'):
-        out += '<li class="menu-item" >'
-        # out += '<a href="{0}">{1}</a>'.format(submenu_item.url, submenu_item.label)
-        out += f'<a href="{submenu_item.get_absolute_url(context.request)}">{submenu_item.title}</a>'
-        # out += draw_menu_children(submenu_item)
-        out += '</li>'
-    out += '</ul>'
-    return out
-
-
-def draw_menu_parents(context, menu_item):
-    out = ''
-    # drawing the sub items
-    out += '<ul class="submenu">'
-    print(menu_item.slug)
-    for submenu_item in MenuItem.objects.filter(children__slug=menu_item.slug):
-        print(submenu_item)
+    for submenu_item in menu_item.children.all().order_by('id'):
         out += '<li class="menu-item" >'
         out += f'<a href="{submenu_item.get_absolute_url(context.request)}">{submenu_item.title}</a>'
         out += '</li>'
-    out += '</ul>'
     return out
 
 
+def get_grandparents(selected_item, context):
+    result = [selected_item]
+    if selected_item.parent:
+        result.extend(get_grandparents(selected_item.parent, context))
+    return result
+
+
+def draw_branch(path, context):
+    out = f"""<li class="menu-item"><a href="{path[0].get_absolute_url(context.request)}">{path[0].title}</a>"""
+    for p in path[1:]:
+        out += '<ul class="submenu">'
+        out += '<li class="menu-item" >'
+        out += f'<a href="{p.get_absolute_url(context.request)}">{p.title}</a>'
+    out += draw_menu_children(context, path[-1])
+    out += len(path)*'</ul>'
+    return out
--- menu/views.py
+++ menu/views.py
@@ -10,23 +10,18 @@ def query_debugger(func):
     @functools.wraps(func)
     def inner_func(*args, **kwargs):
         reset_queries()
-
         start_queries = len(connection.queries)
-
         start = time.perf_counter()
         result = func(*args, **kwargs)
         end = time.perf_counter()
-
         end_queries = len(connection.queries)
-
         print(f"Function : {func.__name__}")
         print(f"Number of Queries : {end_queries - start_queries}")
         print(f"Finished in : {(end - start):.2f}s")
         return result
-
     return inner_func
 
 
 @query_debugger
-def index(request, menu_item_slug=None):
+def index(request):
     return render(request, 'base.html')
